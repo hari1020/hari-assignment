2f6019b889c1da8572852e0dc8b629791d333746
Hari Prasad <123082020+hari1020@users.noreply.github.com>
2023-01-23 13:11:31 +0530

    Update Assignment1.java

--- src/com/testNG/Assignment1.java
+++ src/com/testNG/Assignment1.java
@@ -9,15 +9,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Assignment1 {
 
 	static WebDriver driver;
 
-	@BeforeClass
+	@BeforeMethod
 	public void initializeBrowser() {
 
 		System.setProperty("webdriver.chrome.driver",
@@ -28,7 +28,7 @@ public class Assignment1 {
 		driver.manage().window().maximize();
 	}
 
-	//@Test
+	@Test(priority=1)
 	public static void validInputs() {
 
 		try {
@@ -40,13 +40,13 @@ public class Assignment1 {
 			driver.findElement(By.xpath("//button[text()=' Login ']")).click();
 			WebElement user = driver.findElement(By.xpath("//a[text()=' Test']"));
 			System.out.println(user.getText() + " user logged in Successfully");
-		} catch (Exception e) {
-
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 
 	}
 
-	//@Test
+	@Test(priority=2)
 	public static void invalidInputs() {
 
 		try {
@@ -58,31 +58,32 @@ public class Assignment1 {
 			driver.findElement(By.xpath("//button[text()=' Login ']")).click();
 			WebElement errorMessage = driver.findElement(By.cssSelector("h2#swal2-title"));
 			System.out.println(errorMessage.getText());
-		} catch (Exception e) {
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 
 	}
-	@Test
+	@Test(priority=3)
 	public static void assignment2() {
 
 		try {
-			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+			WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 			WebElement element1 = wait.until(
 					ExpectedConditions.elementToBeClickable
 					(By.xpath("//div[@class='lg'] //button[text()=' 6 MONTHS ']")));
 			Actions act = new Actions(driver);
 			act.moveToElement(element1).perform();
 			element1.click();
-		} catch (Exception e) {
-			
+		} catch (Throwable e) {
+			e.printStackTrace();
 		}
 
 	}
 
-	//@AfterClass
+	@AfterMethod
 	public void tearDown() {
 
-		driver.close();
+		driver.quit();
 	}
 
 }
